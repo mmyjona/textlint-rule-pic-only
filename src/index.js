c9ef5dd81e80b0b299ae705e023878d48b01c0f3
发散 <sheng.gs@antfin.com>
2022-03-25 15:12:55 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ export default function(context, options = {}) {
     const {Syntax, RuleError, report, getSource} = context;
     return {
         [Syntax.Image](node){ // "Str" node
-            const contentLenOtherThanImage = node.parent.raw.replace(node.raw, "").trim().length;
-            if (contentLenOtherThanImage == 0) {
-                const indexOfBugs = node.index;
-                const ruleError = new RuleError("内容仅包含图片", {
-                    index: indexOfBugs // padding of index
-                });
-                report(node, ruleError);
-            }
+            let parent = node.parent;
+            // console.log(parent.parent.type);
+            if (parent.parent.type === "ListItem") {
+                const contentLenOtherThanImage = parent.raw.replace(node.raw, "").trim().length;
+                // console.log("c: " + parent.raw.replace(node.raw, "").trim());
+                if (contentLenOtherThanImage == 0) {
+                    const indexOfBugs = node.index;
+                    const ruleError = new RuleError("内容仅包含图片", {
+                        index: indexOfBugs // padding of index
+                    });
+                    report(node, ruleError);
+                }
+            }   
         }
     }
 };
